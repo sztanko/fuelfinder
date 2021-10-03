b9a0c4f54b4c8314af4e330abc78877b81a53a00
Dimi <demeter.sztanko@compasspathways.com>
2021-10-04 00:17:35 +0100

    Always fetch fresh stations.json

--- fuelfinder/src/lib/data.js
+++ fuelfinder/src/lib/data.js
@@ -2,7 +2,8 @@ import axios from "axios";
 const STATIONS_URL = "stations.json";
 const SAVE_URL = process.env.REACT_APP_SAVE_API_URL;
 const loadStations = (callback, errorCallback) => {
-  axios.get(STATIONS_URL).then(callback).catch(errorCallback);
+  const url = `${STATIONS_URL}?t=${new Date().getTime()}`;
+  axios.get(url).then(callback).catch(errorCallback);
 };
 
 const saveStation = (data, callback) => {
